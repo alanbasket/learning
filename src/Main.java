b24db92d2df6e642839a3b4f3f3fc7bbcf25172a
Баскаева Гелена <baskaevagelena@MacBook-Air-Baskaeva.local>
2023-10-10 21:38:10 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+      public double priceCalculation(double price, int count) {
+          price = 1030;
+          count = 800;
+          return price*count;
+      }
+
+    public static void main(String[] args) {
+
+    }
+}
+
